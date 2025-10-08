58a08a06a5bf6c73ac0cee4ca48c1f0a6022c012
Breanna Taffe <162270025+BreTaffe@users.noreply.github.com>
2025-10-07 23:47:35 -0400

    Update 2 Variables, Types, Operators app.js

--- app.js
+++ app.js
@@ -1,2 +1,15 @@
 console.log("Welcome to PA02: JavaScript (Part 2)!");
 console.log("Repo URL: https://github.com/BreTaffe/pa02");
+
+const name = "Breanna";                
+const age = 23;                  
+const isStudent = true;          
+const hobbies = ["Gamiing", "Movies", "Reading"]; 
+const profile = { major: "Computer Science", year: 4 }; 
+let address = null;         
+
+const nextYear = age + 1;        // arithmetic
+const isAdult = age === 23;    // strict comparison
+const canAccess = isStudent && age >= 18; // logical
+
+console.log(nextYear, isAdult, canAccess);
