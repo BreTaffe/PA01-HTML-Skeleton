d2de83731026434c4a8559f70ce692c5ba55cb51
Breanna Taffe <162270025+BreTaffe@users.noreply.github.com>
2025-10-07 23:54:47 -0400

    Update 3 Control Flow app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 console.log("Welcome to PA02: JavaScript (Part 2)!");
-console.log("Repo URL: https://github.com/BreTaffe/pa02");
+console.log("Repo URL: https://github.com/BreTaffe/PA01-HTML-Skeleton.git");
 
 const name = "Breanna";                
 const age = 23;                  
@@ -13,3 +13,27 @@ const isAdult = age === 23;    // strict comparison
 const canAccess = isStudent && age >= 18; // logical
 
 console.log(nextYear, isAdult, canAccess);
+
+const form = document.getElementById("contactForm");
+const emailInput = document.getElementById("email");
+const status = document.getElementById("status");
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const email = emailInput.value;
+
+  if (email.includes("@") && email.includes(".")) {
+    status.textContent = "✅ Valid email!";
+    status.style.color = "green";
+  } else {
+    status.textContent = "❌ Invalid email format.";
+    status.style.color = "red";
+  }
+});
+
+const hobbyList = document.getElementById("hobbyList");
+for (let i = 0; i < hobbies.length; i++) {
+  const li = document.createElement("li");
+  li.textContent = hobbies[i];
+  hobbyList.appendChild(li);
+}
